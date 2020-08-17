05e81e2fd3ee82b779094f903224ef643145ad3a
RITIK JAIN <jritik324@gmail.com>
2020-08-18 00:00:16 +0530

    Update twitter_sentimental_analysis.py

--- twitter_sentimental_analysis.py
+++ twitter_sentimental_analysis.py
@@ -40,7 +40,7 @@ for i in range(0,10000):
 #Cleaning of Test dataset    
 testing = []
 for i in range(0,3062):
-    review = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",dataset['tweet'][i])
+    review = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",df['tweet'][i])
     review = review.lower()
     review = review.split()
     review = [word for word in review if not word in stopwords.words('english')]
